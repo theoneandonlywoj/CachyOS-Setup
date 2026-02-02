bd82ea68188f26686b790ad0437672594c78ac29
theoneandonlywoj <theoneandonlywoj@gmail.com>
2026-02-02 23:28:56 +0100

    fix: Emacs Locale Issue not using English

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; Force English locale for dates/times (e.g. day names like "Tuesday" in journal filenames and modeline)
+(setq system-time-locale "C")
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
